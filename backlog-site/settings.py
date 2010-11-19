8503bff5d0a6e2a261e5d2208ebec394a2a0f330
Marc Hughes <marc.hughes@gmail.com>
2010-11-19 02:10:58 +0000

    Turnign debug back on

--- backlog-site/settings.py
+++ backlog-site/settings.py
@@ -11,7 +11,7 @@ PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 # tells Pinax to use the default theme
 PINAX_THEME = 'default'
 
-DEBUG = False 
+DEBUG = True 
 TEMPLATE_DEBUG = DEBUG
 
 # tells Pinax to serve media through django.views.static.serve.
