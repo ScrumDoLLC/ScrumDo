adc63faf05657d5e94f610a8c57bd4d2d7562b33
Marc Hughes <mhughes@tomsnyder.com>
2010-11-26 11:59:33 -0500

    We weren't using this app, removing it.

--- backlog-site/apps/tasks/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from attachments.admin import AttachmentInlines
-from tasks.models import Task
-
-class TaskOptions(admin.ModelAdmin):
-    inlines = [AttachmentInlines]
-
-admin.site.register(Task, TaskOptions)
--- backlog-site/apps/tasks/tests/__init__.py
+++
@@ -1,4 +0,0 @@
-from test_authentication import *
-from test_client import *
-from test_models import *
-from test_workflow import *
