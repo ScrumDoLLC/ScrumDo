b9c021be58f3130eb17a1605269db7f31409cf8e
Marc Hughes <mhughes@tomsnyder.com>
2010-11-26 12:00:53 -0500

    Adding admin interface for some of our new models

--- backlog-site/apps/projects/admin.py
+++ backlog-site/apps/projects/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from projects.models import Project
+from projects.models import Project, Iteration, SiteStats, Story, StoryTag
 
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug', 'creator', 'created')
 
 admin.site.register(Project, ProjectAdmin)
+admin.site.register(Iteration)
+admin.site.register(SiteStats )
+admin.site.register(Story )
+admin.site.register(StoryTag )
