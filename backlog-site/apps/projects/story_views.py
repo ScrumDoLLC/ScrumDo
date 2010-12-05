52309d2944ea104853b1818fea2558a8d90d5966
Marc Hughes <mhughes@tomsnyder.com>
2010-12-05 00:12:16 -0500

    Added some documentation to the code

--- backlog-site/apps/projects/story_views.py
+++ backlog-site/apps/projects/story_views.py
@@ -27,6 +27,8 @@ from projects.access import *
 
 
 
+# View called via ajax on the iteration or iteration planning pages.  Meant to set the status of 
+# a story, and then return an html snippet that can be replaced on the page with the new status
 @login_required
 def set_story_status( request, group_slug, story_id, status):
   story = get_object_or_404( Story, id=story_id )
@@ -42,7 +44,10 @@ def set_story_status( request, group_slug, story_id, status):
     }, context_instance=RequestContext(request))
 
 
-  
+# Deletes a story.  No further confirmation, don't post here if you want your story.
+# We're doing some confirmation client-side.  Sometimes called via ajax, sometimes
+# a normal request.  If a normal request, there should be a GET param specifying where
+# to send us to next.
 @login_required
 def delete_story( request, group_slug, story_id ):
   if request.method == "POST":
@@ -94,6 +99,8 @@ def reorder_story( request, group_slug, story_id):
   return  HttpResponse("Fail")
   
 
+# On the iteration planning page, this renders one story view.  Generally called
+# via ajax.
 @login_required
 def mini_story( request, group_slug, story_id):
   story = get_object_or_404( Story, id=story_id )
@@ -103,16 +110,19 @@ def mini_story( request, group_slug, story_id):
     }, context_instance=RequestContext(request))
 
 
-# calculates the tank a new story should have for a give project for 3 general rankings.
+# calculates the rank a new story should have for a project based off of 3 general rankings.
 # 0=top, 1=middle, 2=bottom
 def calculate_rank( project, general_rank ):
   if( general_rank == 0):
-    return 0
+    return 0 
   if( general_rank == 1):
     return round( project.stories.all().count() / 2)
   return project.stories.all().count()+1
 
 
+# Returns the edit-story form, with minimal html wrapper.  This is useful for displaying within
+# a facebox popup.  
+# One place it's used is on the iteration page when you click the magnifying glass for a story.
 @login_required
 def story( request, group_slug, story_id ):
   story = get_object_or_404( Story, id=story_id )
@@ -146,6 +156,8 @@ def story( request, group_slug, story_id ):
       "return_type": return_type
     }, context_instance=RequestContext(request))
 
+# Returns the stories for a given iteration as an html snippet.  The iteration planning page uses this
+# uplon load, and then also upon filtering by the user
 @login_required
 def stories_iteration(request, group_slug, iteration_id):
   project = get_object_or_404(Project, slug=group_slug)  
@@ -176,6 +188,9 @@ def stories_iteration(request, group_slug, iteration_id):
   }, context_instance=RequestContext(request))
 
 
+# The iteration planning tool.  It can also handle the add story form.
+# TODO: We should factor out the add story form functionality
+# TODO: We should rename this method, and likely rename the URL that points at it as well.
 @login_required
 def stories(request, group_slug):
   project = get_object_or_404(Project, slug=group_slug)  
@@ -205,11 +220,13 @@ def stories(request, group_slug):
 
 
 # Returns the next appropriate local_id for a given project.
+# TODO: should this be in the project model?  Or at least somewhere else.
 def getNextId( project ):
   if project.stories.count() == 0:
     return 1
   return project.stories.order_by('-local_id')[0].local_id + 1
 
+# Handles the excel import.
 @login_required
 def import_file(request, group_slug):
   project = get_object_or_404(Project, slug=group_slug)
@@ -241,6 +258,8 @@ def processImport( project, file , user):
     story.save()
   user.message_set.create(message=("%d stories imported" % count))
 
+# Returns an html snippet that we use for a read-only full view of the story.  Right now, this is used
+# when you mouse-hover over the eye icon for a story on an iteration page.  
 def pretty_print_story(request, group_slug, story_id):
   story = get_object_or_404(Story, id=story_id)  
   read_access_or_403( story.project, request.user )
