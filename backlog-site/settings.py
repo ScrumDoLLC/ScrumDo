4664aba7cb046d7072bb6d5f324083c184a3884e
Ajay <ajay@codegenesys.com>
2010-12-08 22:00:12 -0500

    Changed database name and user

--- backlog-site/settings.py
+++ backlog-site/settings.py
@@ -24,8 +24,8 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASE_ENGINE = 'mysql'    # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'ado_mssql'.
-DATABASE_NAME = 'backlog'       # Or path to database file if using sqlite3.
-DATABASE_USER = 'backlog'             # Not used with sqlite3.
+DATABASE_NAME = 'scrumdo'       # Or path to database file if using sqlite3.
+DATABASE_USER = 'scrumaster'             # Not used with sqlite3.
 DATABASE_PASSWORD = 'dlfksj39028'         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
