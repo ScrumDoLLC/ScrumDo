fa097c2775451c13d28afe74b5f09ad52be6fed8
Marc Hughes <mhughes@tomsnyder.com>
2011-01-02 16:10:21 -0500

    Fixed up some copyright info.

--- backlog-site/settings.py
+++ backlog-site/settings.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 
 # ScrumDo - Agile/Scrum story management web application 
-# Copyright (C) 2011 Marc Hughes, Ajay Reddy
+# Copyright (C) 2011 ScrumDo LLC
 # 
-# This library is free software; you can redistribute it and/or
+# This software is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
 # License as published by the Free Software Foundation; either
 # version 2.1 of the License, or (at your option) any later version.
 # 
-# This library is distributed in the hope that it will be useful,
+# This software is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 # Lesser General Public License for more details.
