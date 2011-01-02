60529b6faa6f91d3a6c0d3aaac55b77d4127b371
Marc Hughes <mhughes@tomsnyder.com>
2011-01-02 11:40:16 -0500

    Adding copyright notice / license text.

--- backlog-site/extras_sync.py
+++ backlog-site/extras_sync.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python
 
+# ScrumDo - Agile/Scrum story management web application 
+# Copyright (C) 2011 Marc Hughes, Ajay Reddy
+# 
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+# 
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+# 
+# You should have received a copy (See file COPYING) of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+
+
 # This script handles running the extra's syncronization logic.
 # You should set this up on some sort of scheduled basis.  The
 # scrumdo.com website runs it once a minute.
