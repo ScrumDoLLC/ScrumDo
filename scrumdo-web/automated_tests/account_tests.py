c732d9630d70cc6c9ad879603f2b5369b293d62b
Marc Hughes <mhughes@scrumdo.com>
2011-01-16 11:07:00 -0500

    Selenium test to create an organization and a project inside it.

--- scrumdo-web/automated_tests/account_tests.py
+++ scrumdo-web/automated_tests/account_tests.py
@@ -30,7 +30,37 @@ class AccountTests(unittest.TestCase):
         sel.wait_for_page_to_load("30000")
         try: self.failUnless(sel.is_text_present("Get Scrum Done with ScrumDo"))
         except AssertionError, e: self.verificationErrors.append(str(e))    
-        
+
+    def test_organization_create(self):      
+          sel = self.selenium
+          sel.open("/")
+          sel.click("link=Login")
+          sel.wait_for_page_to_load("30000")
+          sel.type("id_username", "selenium_user")
+          sel.type("id_password", "selenium_password")
+          sel.click(u"//input[@type='submit']")
+          sel.wait_for_page_to_load("30000")
+          sel.click("link=Create New Organization")
+          sel.wait_for_page_to_load("30000")
+          sel.type("id_name", "selenium test org")
+          sel.type("id_slug", "selenium_test_organi")
+          sel.type("id_description", "selenium_test_organization description.")
+          sel.click("//button[@type='submit']")
+          sel.wait_for_page_to_load("30000")
+          try: self.failUnless(sel.is_text_present("Organization Created."))
+          except AssertionError, e: self.verificationErrors.append(str(e))
+          sel.click("link=Create New Project")
+          sel.wait_for_page_to_load("30000")
+          sel.type("id_slug", "selenium_test_proj")
+          sel.type("id_name", "Test project from automated tests")
+          sel.type("id_description", "Test project from automated tests")
+          sel.click("//button[@type='submit']")
+          sel.wait_for_page_to_load("30000")
+          try: self.failUnless(sel.is_text_present("Project Created"))
+          except AssertionError, e: self.verificationErrors.append(str(e))
+          sel.click("link=Logout")
+          sel.wait_for_page_to_load("30000")
+          
     def test_login(self):
         sel = self.selenium
         sel.open("/")
@@ -43,6 +73,8 @@ class AccountTests(unittest.TestCase):
         sel.wait_for_page_to_load("30000")
         try: self.failUnless(sel.is_text_present("Successfully logged in"))
         except AssertionError, e: self.verificationErrors.append(str(e))
+        sel.click("link=Logout")
+        sel.wait_for_page_to_load("30000")        
     
     def tearDown(self):
         self.selenium.stop()
--- scrumdo-web/automated_tests/suite.py
+++ scrumdo-web/automated_tests/suite.py
@@ -7,6 +7,7 @@ suite = unittest.TestSuite()
 suite.addTest(homepage.HomepageTest("test_homepage"))
 suite.addTest(account_tests.AccountTests("test_sign_up"))
 suite.addTest(account_tests.AccountTests("test_login"))
+suite.addTest(account_tests.AccountTests("test_organization_create"))
 
 
 unittest.TextTestRunner(verbosity=2).run(suite)
