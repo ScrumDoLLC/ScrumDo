db6dc86bda035efc5512af16c4b7c50fbad05ce1
Ajay <ajay@scrumdo.com>
2011-01-26 12:47:50 -0500

    added links to stories, iterations, and projects, to newsfeed.

--- scrumdo-web/apps/activities/templatetags/activity_tags.py
+++ scrumdo-web/apps/activities/templatetags/activity_tags.py
@@ -1,4 +1,7 @@
 from django import template
+from django.core.urlresolvers import reverse
+from django.utils.safestring import mark_safe
+
 from activities.models import Activity, StoryActivity, IterationActivity
 
 register = template.Library()
@@ -7,19 +10,20 @@ register = template.Library()
 @register.filter
 def activity_action(activity):
     if isinstance(activity,StoryActivity) and activity.storyactivity.status:
-        return (activity.action.name + " to " + activity.storyactivity.status)
+        return (activity.action.name + " to " + activity.storyactivity.status + " for ")
     else:
         return activity.action.name
 
 @register.filter
 def activity_object(activity):
     if isinstance(activity,StoryActivity):
+        return mark_safe("<a href='%s#story_%s'>" % (reverse("iteration", args=[activity.project.slug, activity.story.iteration.id]), activity.story.id) + activity.story.summary + "</a>")
         return ("Story " + activity.story.summary)
     elif isinstance(activity,IterationActivity):
-        return ("Iteration " + activity.iteration.name)
+        return mark_safe("<a href='%s'>Iteration " % (reverse("iteration", args=[activity.project.slug, activity.iteration.id])) + activity.iteration.name + "</a>")
     else:
-        return ("Project " + activity.project.slug)
+        return mark_safe("<a href='%s'>" % (reverse("project_detail", args=[activity.project.slug])) + activity.project.name + "</a>")
 
 @register.filter
 def activity_context(activity):
-    return ("Project " + activity.project.name)
+        return mark_safe("<a href='%s'>" % (reverse("project_detail", args=[activity.project.slug])) + activity.project.name + "</a>")
