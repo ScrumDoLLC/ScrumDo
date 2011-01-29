bf83f227fcb7857f31de500956f1da4e3637e191
Marc Hughes <mhughes@scrumdo.com>
2011-01-29 16:28:47 -0500

    Added terms of service

--- scrumdo-web/apps/about/urls.py
+++ scrumdo-web/apps/about/urls.py
@@ -4,7 +4,7 @@ from django.views.generic.simple import direct_to_template
 urlpatterns = patterns('',
     url(r'^$', direct_to_template, {"template": "about/about.html"}, name="about"),
     
-    url(r'^terms/$', direct_to_template, {"template": "about/terms.html"}, name="terms"),
+    url(r'^terms/$', direct_to_template, {"template": "terms.html"}, name="terms"),
     url(r'^privacy/$', direct_to_template, {"template": "about/privacy.html"}, name="privacy"),
     url(r'^dmca/$', direct_to_template, {"template": "about/dmca.html"}, name="dmca"),
     
