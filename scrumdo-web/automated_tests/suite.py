95b7a08297087fb85aa85704136e2c5b04e88b40
Ajay <ajay@scrumdo.com>
2011-02-11 12:22:14 -0500

    updating automated tests with more newsfeed tests

--- scrumdo-web/automated_tests/suite.py
+++ scrumdo-web/automated_tests/suite.py
@@ -10,5 +10,6 @@ suite.addTest(account_tests.AccountTests("test_sign_up"))
 suite.addTest(account_tests.AccountTests("test_login"))
 suite.addTest(account_tests.AccountTests("test_organization_create"))
 suite.addTest(newsfeed_tests.NewsfeedTests("test_create_story"))
+suite.addTest(newsfeed_tests.NewsfeedTests("test_newsfeed_activity_create"))
 
 unittest.TextTestRunner(verbosity=2).run(suite)
