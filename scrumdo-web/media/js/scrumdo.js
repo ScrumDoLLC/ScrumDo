324d620c28cdb510fa6474db34fd33668e7adf33
Marc Hughes <mhughes@scrumdo.com>
2011-02-12 21:00:08 -0500

    Iteration planning todo buttons were broken in all browsers, fixed that.  Styled iteration planning to look ok in IE7.

--- scrumdo-web/media/js/scrumdo.js
+++ scrumdo-web/media/js/scrumdo.js
@@ -3,3 +3,17 @@ function setupAutoClose( divID )
 {  
   setTimeout( "$('body').one('click',function() { $(\"" + divID + "\").fadeOut(100); });" , 100);
 }
+
+function setStatus(storyID, project_slug, status, return_type)
+{
+    $(".todoButtons").hide();
+    $.ajax({
+	    url: "/projects/project/" + project_slug + "/story/" + storyID + "/set_" + status,
+		type: "POST",
+		data: ({return_type : return_type}),
+		success: function(responseText) {
+		$("#story_" + storyID).replaceWith(responseText);
+		setUpStoryLinks();
+	    }
+	});
+}
