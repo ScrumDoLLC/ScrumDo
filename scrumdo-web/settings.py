b83592e3f39bf7cb6cec4aaa19932a0a056765ba
Marc Hughes <mhughes@scrumdo.com>
2011-02-20 09:28:53 -0500

    removing scrumdo.com settings from settings.py.  Use local_settings.py to override these.

--- scrumdo-web/settings.py
+++ scrumdo-web/settings.py
@@ -220,8 +220,8 @@ ACCOUNT_EMAIL_VERIFICATION = False
 
 EMAIL_CONFIRMATION_DAYS = 2
 EMAIL_DEBUG = DEBUG
-CONTACT_EMAIL = "scrumdo@scrumdo.com"
-SITE_NAME = "ScrumDo"
+
+SITE_NAME = "ScrumDo Community Site"
 LOGIN_URL = "/account/login/"
 LOGIN_REDIRECT_URLNAME = "projects.views.home"
 
@@ -230,8 +230,11 @@ EMAIL_HOST='localhost'
 EMAIL_HOST_USER=''                  
 EMAIL_HOST_PASSWORD=''        
 EMAIL_PORT='25'
-DEFAULT_FROM_EMAIL = 'noreply@scrumdo.com'   
-SERVER_EMAIL = 'noreply@scrumdo.com'
+
+CONTACT_EMAIL = "help@example.com"
+DEFAULT_FROM_EMAIL = 'noreply@example.com'   
+SERVER_EMAIL = 'noreply@example.com'
+SUPPORT_URL = "http://support.example.com/"
 
 GOOGLE_ANALYTICS = False
 GOOGLE_ANALYTICS_ACCOUNT = ""
@@ -240,7 +243,7 @@ CACHE_BACKEND = 'locmem://'
 
 INTERNAL_IPS = ('127.0.0.1',)
 
-SUPPORT_URL = "http://support.scrumdo.com/"
+
 
 SCRUMDO_EXTRAS = ("extras.plugins.github_issues.GitHubIssuesExtra",
                   "extras.plugins.example.ExampleExtra",)
