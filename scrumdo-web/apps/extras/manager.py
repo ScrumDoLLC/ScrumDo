aa2eba0dc563c83221d74ac06d3df05ea9f6206a
Marc Hughes <mhughes@scrumdo.com>
2011-02-22 12:35:57 -0500

    Changing how plugins are loaded.  Added new section to configuration template.

--- scrumdo-web/apps/extras/manager.py
+++ scrumdo-web/apps/extras/manager.py
@@ -116,12 +116,7 @@ class ExtrasManager:
       self.extras[ extra.getSlug() ] = extra
       
 def get_class( kls ):
-  parts = kls.split('.')
-  module = ".".join(parts[:-1])
-  m = __import__( module )
-  for comp in parts[1:]:
-      m = getattr(m, comp)            
-  return m
-    
+    return getattr(__import__( kls , {}, {}, ['Plugin']), 'Plugin')
+
     
 manager = ExtrasManager( settings.SCRUMDO_EXTRAS )  
