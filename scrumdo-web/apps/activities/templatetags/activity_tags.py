be8ee28868a9d3e83a40dc6f1f80aafb9bb7cb76
Marc Hughes <mhughes@scrumdo.com>
2011-05-05 20:55:58 -0400

    Can't have the activity feed causing a home page error.

--- scrumdo-web/apps/activities/templatetags/activity_tags.py
+++ scrumdo-web/apps/activities/templatetags/activity_tags.py
@@ -18,7 +18,7 @@ def iteration_name(iteration):
             return "Iteration %s" % iteration.name
 
 def iteration_uri(iteration, project):
-    return reverse("iteration", args=[project.slug, iteration.id])
+        return reverse("iteration", args=[project.slug, iteration.id])
 
 def iteration_link(iteration, project):
     return ("<a href='%s'>" % iteration_uri(iteration, project)) + iteration_name(iteration) + "</a>"
@@ -37,54 +37,65 @@ def smart_truncate(content, length=100, suffix='...'):
 
 @register.filter
 def subscription_checkbox(project , subscription_list):
-    if project.id in subscription_list:
-        return "<input type=\"checkbox\" name=\"subscriptions\" value=\"%d\" checked=\"checked\"> " % project.id
-    else:
-        return "<input type=\"checkbox\" name=\"subscriptions\" value=\"%d\"> " % project.id
-
+    try:
+        if project.id in subscription_list:
+            return "<input type=\"checkbox\" name=\"subscriptions\" value=\"%d\" checked=\"checked\"> " % project.id
+        else:
+            return "<input type=\"checkbox\" name=\"subscriptions\" value=\"%d\"> " % project.id
+    except:
+        return ""
 
 @register.filter
 def activity_action(activity):
-    if isinstance(activity,StoryActivity) and activity.status:
-        return (activity.action.name + " to " + activity.status )
-    elif isinstance(activity,IterationActivity) and activity.numstories:
-        return activity.action.name + " " + str(activity.numstories) + " stories"
-    else:
-        return activity.action.name
+    try:
+        if isinstance(activity,StoryActivity) and activity.status:
+            return (activity.action.name + " to " + activity.status )
+        elif isinstance(activity,IterationActivity) and activity.numstories:
+            return activity.action.name + " " + str(activity.numstories) + " stories"
+        else:
+            return activity.action.name
+    except:
+        return ""
 
 @register.filter
 def activity_object(activity):
-    if isinstance(activity,StoryActivity):
-        s = activity.story
-        if activity.status:
-            return mark_safe(" for " + story_link(s, activity.project) + " in " + iteration_link(s.iteration, activity.project))
-        else:
-            return mark_safe(story_link(s, activity.project) + " in " + iteration_link(s.iteration, activity.project))
+    try:
+        if isinstance(activity,StoryActivity):
+            s = activity.story
+            if activity.status:
+                return mark_safe(" for " + story_link(s, activity.project) + " in " + iteration_link(s.iteration, activity.project))
+            else:
+                return mark_safe(story_link(s, activity.project) + " in " + iteration_link(s.iteration, activity.project))
         
         
-    elif isinstance(activity,IterationActivity):
-        if activity.numstories:
-            # this is a reorder activity
-            start = "in "
+        elif isinstance(activity,IterationActivity):
+            if activity.numstories:
+                # this is a reorder activity
+                start = "in "
+            else:
+                start = ""
+            return mark_safe(start + iteration_link(activity.iteration, activity.project))
+        elif isinstance(activity,DeletedActivity):
+            return mark_safe(activity.name)
+        elif isinstance(activity,CommentActivity):
+            return mark_safe("<em>" + escape(activity.comment) + "</em>")
+        elif isinstance(activity,PointsChangeActivity):
+            return mark_safe("from <strong>" + escape(activity.old) + "</strong> to <strong>" + escape(activity.new) + "</strong> for " + story_link(activity.story, activity.project))
         else:
-            start = ""
-        return mark_safe(start + iteration_link(activity.iteration, activity.project))
-    elif isinstance(activity,DeletedActivity):
-        return mark_safe(activity.name)
-    elif isinstance(activity,CommentActivity):
-        return mark_safe("<em>" + escape(activity.comment) + "</em>")
-    elif isinstance(activity,PointsChangeActivity):
-        return mark_safe("from <strong>" + escape(activity.old) + "</strong> to <strong>" + escape(activity.new) + "</strong> for " + story_link(activity.story, activity.project))
-    else:
-        return mark_safe("<a href='%s'>" % (reverse("project_detail", args=[activity.project.slug])) + activity.project.name + "</a>")
+            return mark_safe("<a href='%s'>" % (reverse("project_detail", args=[activity.project.slug])) + activity.project.name + "</a>")
+    except:
+        return ""
 
 @register.filter
 def activity_context(activity):
-    if isinstance(activity,IterationActivity) and activity.numstories:
-        start = "of"
-    if isinstance(activity,CommentActivity):
-        start = "on " + story_link(activity.story, activity.project) + " in"
-    else:
-        start = "in"
-    url = reverse("project_detail", args=[activity.project.slug])
-    return mark_safe( "%s <a href='%s'>%s</a>" % (start, url, activity.project.name )  )
+    try:
+        if isinstance(activity,IterationActivity) and activity.numstories:
+            start = "of"
+        if isinstance(activity,CommentActivity):
+            start = "on " + story_link(activity.story, activity.project) + " in"
+        else:
+            start = "in"
+        url = reverse("project_detail", args=[activity.project.slug])
+        return mark_safe( "%s <a href='%s'>%s</a>" % (start, url, activity.project.name )  )
+    except:
+        return ""
