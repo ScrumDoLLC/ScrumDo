77023a7cbc746c31d94cbe5c9aa73e583aa69daf
Marc Hughes <mhughes@scrumdo.com>
2011-05-15 14:55:34 -0400

    Removed activity loading from homepage view, now runs an ajax call when the home page loads.  Results in quicker load times for the initial parts of the homepage.  Also increased the number of stories loaded from 10 to 40

--- scrumdo-web/apps/projects/views.py
+++ scrumdo-web/apps/projects/views.py
@@ -80,19 +80,13 @@ def home( request ):
     my_projects = [];
     member_projects = [];
     organizations = [];
-    activities=[]
     next_page = False
 
     if request.user.is_authenticated():
         organizations = Organization.getOrganizationsForUser(request.user)
-        activities = Activity.getActivitiesForUser(request.user)
 
         assigned_stories = Story.getAssignedStories(request.user)
 
-        paginator = Paginator(activities, 20)
-        page_obj = paginator.page(1)
-        activities = page_obj.object_list
-        next_page = page_obj.has_next()
 
         memberships = ProjectMember.objects.filter( user=request.user )
         for membership in memberships:
@@ -110,8 +104,6 @@ def home( request ):
         return render_to_response("homepage.html", {
            "my_projects":my_projects,
            "my_organizations": organizations,
-           "activities": activities,
-           "activities_next_page":next_page,
            "assigned_stories": assigned_stories,
            "return_type" : "queue", # for the queue mini stories
            "member_projects":member_projects,
