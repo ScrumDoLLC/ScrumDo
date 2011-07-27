abfe32462a30350932a0b28c38bb696353c8f79f
Marc Hughes <mhughes@tomsnyder.com>
2011-07-27 19:33:21 -0400

    Wording change

--- scrumdo-web/apps/projects/iteration_views.py
+++ scrumdo-web/apps/projects/iteration_views.py
@@ -147,8 +147,9 @@ def delete_iteration( request, group_slug, iteration_id ):
     iteration = get_object_or_404( Iteration, id=iteration_id )
 
     if iteration.default_iteration:
-        request.user.message_set.create(message="You can not delete the default iteration.")
-        HttpResponseRedirect( reverse('iteration', kwargs={'group_slug':iteration.project.slug, 'iteration_id':iteration.id }) ) #redirect to same iteration+display msg., as delete failed
+        request.user.message_set.create(message="You can not delete your default iteration.")
+        return HttpResponseRedirect( reverse('iteration', kwargs={'group_slug':iteration.project.slug, 'iteration_id':iteration.id }) ) #redirect to same iteration+display msg., as delete failed
+        
 
     if request.method == "POST":
         write_access_or_403(iteration.project,request.user)
